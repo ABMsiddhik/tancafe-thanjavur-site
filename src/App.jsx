60cefd0a557a7e9bd2b32b094ca7163c42b9fed7
ABMsiddhik <abmsiddhik@gmail.com>
2025-07-22 10:15:55 +0530

    _redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopBar from './components/TopBar';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
